9ae8b698691f1f80c324eb77c9198980c7651369
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2023-06-14 16:24:42 +0530

    add api + lamda + auth + graphql

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "inspirationalquotes": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "inspirationalquotesbb4d5b59": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "inspoquoteLamda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
-import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 import Head from "next/head";
+import { useState } from "react";
+
+// Components
+import {
+  BackgroundImage1,
+  BackgroundImage2,
+  FooterCon,
+  FooterLink,
+  GenerateQuoteButton,
+  GenerateQuoteButtonText,
+  GradientBackgroundCon,
+  QuoteGeneratorCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorSubTitle,
+  QuoteGeneratorTitle,
+  RedSpan,
+} from "@/components/QuoteGenerator/QuoteGeneratorElements";
+
+// Assets
+import Clouds1 from "../assets/cloud-and-thunder.png";
+import Clouds2 from "../assets/cloudy-weather.png";
 
 export default function Home() {
+  const [numberofQuotes, setnumberofQuotes] = useState<Number | null>(0);
   return (
     <>
       <Head>
@@ -11,7 +32,57 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <GradientBackgroundCon></GradientBackgroundCon>
+      <GradientBackgroundCon>
+        {/* Quote Generator Modal Pop-up */}
+        {/* <QuoteGeneratorModal /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+
+            <QuoteGeneratorSubTitle>
+              Looking for a splash of inspiration? Generate a quote card with a
+              random inspirational quote provided by{" "}
+              <FooterLink
+                href="https://zenquotes.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ZenQuotes API
+              </FooterLink>
+              .
+            </QuoteGeneratorSubTitle>
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Make a quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images */}
+        <BackgroundImage1 src={Clouds1} height="300" alt="cloudybackground1" />
+        <BackgroundImage2 src={Clouds2} height="300" alt="cloudybackground2" />
+
+        {/* Footer */}
+        <FooterCon>
+          <>
+            Quotes Generated:{numberofQuotes}
+            <br />
+            Developed with <RedSpan>♥</RedSpan> by{" "}
+            <FooterLink
+              href="https://github.com/abhijitmanna912001"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Abhijit Manna
+            </FooterLink>
+          </>
+        </FooterCon>
+      </GradientBackgroundCon>
     </>
   );
 }
